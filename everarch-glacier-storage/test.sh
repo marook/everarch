46884916ced4cc628f4c5341b0b776c1c8d26eda
Markus Peröbner <markus.peroebner@gmail.com>
2022-01-26 15:17:24 +0100

    exclude concurrent-glacier-test from valgrind runs
    because it's too slow.

--- everarch-glacier-storage/test.sh
+++ everarch-glacier-storage/test.sh
@@ -6,6 +6,12 @@ ctest
 echo "Start valgrind testing…"
 for test in *-test
 do
+    if [[ "${test}" == "concurrent-glacier-test" ]]
+    then
+        # the tests above are currently excluded from the valgrind
+        # runs because they run too slow.
+        continue
+    fi
     echo "Testing ${test}…"
     valgrind --quiet --leak-check=yes "./${test}"
 done
