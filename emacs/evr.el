4e27b9fd98c20afbc012522fd8ef9f5c093f3999
Markus Peröbner <markus.peroebner@gmail.com>
2022-05-05 20:58:45 +0200

    extend the evr emacs integration
    - add ability to jump to previous and next claim ref in search buffers
    - x will now follow a claim ref to it's claim xml
    - c or f will now follow a claim ref to it's file content
    - enter on a claim ref will now perform a search for that ref
    - claim xml or file contents buffers will now be replaced on follow up
      visit of the same evr resource. there is a confirmation for unsaved
      changes.
    - add evr-save-claim-set function to persist claim-set
    - add hooks for after file claim creation and after claim-set creation

--- emacs/evr.el
+++ emacs/evr.el
@@ -34,7 +34,6 @@ evr-attr-index and prints the results into a new buffer."
       (let ((moving (= (point) (process-mark proc)))
             (ro inhibit-read-only))
         (save-excursion
-          ;; Insert the text, advancing the process marker.
           (goto-char (process-mark proc))
           (setq inhibit-read-only t)
           (insert string)
@@ -60,19 +59,43 @@ evr-attr-index and prints the results into a new buffer."
                   (point))))
         (con (open-network-stream "evr-attr-index-search" (buffer-name) "localhost" 2362)))
     (set-process-filter con 'evr--terminate-on-message-end-filter)
+    (set-process-sentinel
+     con
+     (lambda (con event)
+       ;; prevent process status from being printed to the search
+       ;; results buffer
+       nil))
     (process-send-string con (concat "s select * where " query "\n"))))
 
 (defvar evr-attr-index-results-mode-map
   (let ((map (make-keymap)))
     (set-keymap-parent map special-mode-map)
+    (define-key map "p" 'evr-previous-claim-ref)
+    (define-key map " " 'evr-next-claim-ref)
+    (define-key map "n" 'evr-next-claim-ref)
     (define-key map "C" 'kill-current-buffer)
     (define-key map "r" 'isearch-backward)
     (define-key map "s" 'isearch-forward)
     (define-key map "g" 'evr-attr-index-search-from-buffer)
-    (define-key map "f" 'evr-follow-claim-ref)
-    (define-key map "\C-m" 'evr-follow-claim-ref)
+    (define-key map "x" 'evr-follow-claim-ref-xml)
+    (define-key map "\C-m" 'evr-follow-claim-ref-search)
+    (define-key map "c" 'evr-follow-claim-ref-contents)
+    (define-key map "f" 'evr-follow-claim-ref-contents)
     map)
   "Local keymap for evr-attr-index-results-mode buffers.")
+
+(defun evr-previous-claim-ref ()
+  (interactive)
+  (re-search-backward evr--claim-ref-pattern nil t))
+
+(defun evr-next-claim-ref ()
+  (interactive)
+  (let ((point-before (point)))
+    (unless (eobp)
+      (forward-char 1))
+    (if (eq (re-search-forward evr--claim-ref-pattern nil t) nil)
+        (goto-char point-before)
+      (re-search-backward evr--claim-ref-pattern))))
 
 (defface evr-claim-ref
   '((t (:inherit link)))
@@ -133,73 +156,107 @@ evr-attr-index and prints the results into a new buffer."
       (end-of-line)
       (buffer-substring-no-properties start (point)))))
 
-(defun evr-find-claim (claim-ref seed-ref)
-  "Visit claim with given claim ref."
-  (interactive "sRef: ")
-  (let ((old-claim-buffer (get-buffer claim-ref)))
-    (if old-claim-buffer (kill-buffer old-claim-buffer)))
-  (let ((claim-ref-buffer (get-buffer-create claim-ref)))
-    (make-process
-     :name (concat "evr get-claim " claim-ref)
-     :buffer claim-ref
-     :command `("evr" "get-claim" ,claim-ref)
-     :sentinel
-     (lambda (process event)
-       (if (string= event "finished\n")
-           (with-current-buffer claim-ref-buffer
-             (set-buffer-modified-p nil)
-             (normal-mode)
-             (setq-local evr-seed-ref seed-ref)
-             (setq-local evr-claim-ref claim-ref)
-             )))
-     :stderr (get-buffer-create "*evr get-claim errors*")
-     )
-    (switch-to-buffer claim-ref)))
+(defun evr--remove-buffer (buffer-name)
+  "Kills a buffer if it exists and asks if it is modified.
 
-(defun evr-follow-claim ()
-  "Follows the root claim within the current buffer."
-  (interactive)
-  (let ((claim-doc (libxml-parse-xml-region (point-min) (point-max))))
-    ;; TODO add xml namespace handling here
-    (if (string= (symbol-name (car claim-doc)) "file")
-        (let ((file-attrs (car (cdr claim-doc)))
-              (title evr-claim-ref))
-          (if file-attrs
-              (let ((title-attr-value (alist-get 'title file-attrs)))
-                (if title-attr-value
-                    (setq title title-attr-value))))
-          (evr--follow-file-claim evr-claim-ref evr-seed-ref title))
-      (message "No idea how to follow this claim"))
-  ))
+Returns t if no such buffer exists or it was successfully
+killed. Returns nil otherwise."
+  (if (get-buffer buffer-name)
+      (with-current-buffer buffer-name
+        (if (buffer-modified-p)
+            (kill-buffer-ask (get-buffer buffer-name))
+          (kill-buffer)))
+    t))
+
+(defun evr--visit-claim-xml (claim-ref seed-ref &optional complete)
+  "Visit claim XML with given claim ref in a buffer.
+
+Returns the buffer's name."
+  (if (evr--remove-buffer claim-ref)
+      (let ((claim-ref-buffer (get-buffer-create claim-ref)))
+        (make-process
+         :name (concat "evr get-claim " claim-ref)
+         :buffer claim-ref
+         :command `("evr" "get-claim" ,claim-ref)
+         :sentinel
+         (lambda (process event)
+           (if (string= event "finished\n")
+               (with-current-buffer claim-ref-buffer
+                 (set-buffer-modified-p nil)
+                 (normal-mode)
+                 (setq-local evr-seed-ref seed-ref)
+                 (setq-local evr-claim-ref claim-ref)
+                 (if complete
+                     (funcall complete claim-ref))
+                 )))
+         :stderr (get-buffer-create "*evr get-claim errors*")
+         )
+        claim-ref)))
 
 (defun evr--follow-file-claim (claim-ref seed-ref buffer-name)
-  ;; TODO assert evr-claim-ref buffer local variable is set
   (message "evr get %s" buffer-name)
-  (make-process
-   :name (concat "evr get-file " claim-ref)
-   :buffer buffer-name
-   :command `("evr" "get-file" ,claim-ref)
-   :sentinel
-   (lambda (process event)
-     (if (string= event "finished\n")
-         (with-current-buffer buffer-name
-           ;; TODO should we call set-visited-file-name?
-           (set-buffer-modified-p nil)
-           (goto-char (point-min))
-           (normal-mode)
-           (setq-local evr-seed-ref seed-ref)
-           (setq-local evr-claim-ref claim-ref)
-           (evr-file-claim-mode nil)
-           (switch-to-buffer buffer-name)
-           )))
-   :stderr (get-buffer-create "*evr get-file errors*")
-   ))
+  (if (evr--remove-buffer buffer-name)
+      (make-process
+       :name (concat "evr get-file " claim-ref)
+       :buffer buffer-name
+       :command `("evr" "get-file" ,claim-ref)
+       :sentinel
+       (lambda (process event)
+         (if (string= event "finished\n")
+             (with-current-buffer buffer-name
+               (set-buffer-modified-p nil)
+               (goto-char (point-min))
+               (normal-mode)
+               (setq-local evr-seed-ref seed-ref)
+               (setq-local evr-claim-ref claim-ref)
+               (evr-file-claim-mode nil)
+               (switch-to-buffer buffer-name)
+               )))
+       :stderr (get-buffer-create "*evr get-file errors*")
+       )))
 
-(defun evr-follow-claim-ref ()
-  "Follows the claim ref at point."
+(defun evr-follow-claim-ref-xml ()
+  "Follows the claim ref at point and will show it's claim XML in
+a new buffer."
   (interactive)
-  nil
-  (evr-find-claim (evr--get-claim-ref) (evr--get-seed-ref)))
+  (let ((buffer-name (evr--visit-claim-xml (evr--get-claim-ref) (evr--get-seed-ref))))
+    (if buffer-name
+        (switch-to-buffer buffer-name))))
+
+(defun evr-follow-claim-ref-search ()
+  "Follows the claim ref at point and will show it's attributes
+in a new buffer."
+  (interactive)
+  (evr-attr-index-search (concat "ref=" (evr--get-claim-ref))))
+
+(defun evr-follow-claim-ref-contents ()
+  "Follows the claim ref at point.
+
+Will show the file contents in a new buffer if the claim ref
+points to a file claim."
+  (interactive)
+  (evr--visit-claim-xml
+   (evr--get-claim-ref)
+   (evr--get-seed-ref)
+   (lambda (buffer-name)
+     (evr-follow-claim buffer-name))))
+
+(defun evr-follow-claim (claim-buffer)
+  "Follows the root claim within the current buffer."
+  (interactive "bClaim: ")
+  (with-current-buffer claim-buffer
+    (let ((claim-doc (libxml-parse-xml-region (point-min) (point-max))))
+      ;; TODO add xml namespace handling here
+      (if (string= (symbol-name (car claim-doc)) "file")
+          (let ((file-attrs (car (cdr claim-doc)))
+                (title evr-claim-ref))
+            (if file-attrs
+                (let ((title-attr-value (alist-get 'title file-attrs)))
+                  (if title-attr-value
+                      (setq title title-attr-value))))
+            (evr--follow-file-claim evr-claim-ref evr-seed-ref title))
+        (message "No idea how to follow the claim in buffer %s" (buffer-name)))
+      )))
 
 (defun evr-file-claim-mode (&optional arg)
   "This minor mode makes a buffer an everarch sourced file."
@@ -212,29 +269,76 @@ evr-attr-index and prints the results into a new buffer."
         (evr-enable-file-claim-mode)
       (evr-disable-file-claim-mode))))
 
+(defun evr-enable-file-claim-mode ()
+  (add-hook 'write-contents-functions 'evr-save-file nil t))
+
+(defun evr-disable-file-claim-mode ()
+  (remove-hook 'write-contents-finctions 'evr-save-file t))
+
+(defcustom evr-seed-file-claim-saved-hook nil
+  "Run after `evr-save-file' saved a seed file claim.
+
+The claim's seed and claim ref are passed as arguments. The seed
+ref is always nil.
+
+(add-hook
+ 'evr-seed-file-claim-saved-hook
+ (lambda (seed-ref claim-ref)
+   …
+))
+"
+  :group 'evr
+  :type 'hook)
+
+(defcustom evr-file-claim-saved-hook nil
+  "Run after `evr-save-file' saved a file claim.
+
+The claim's seed and claim ref are passed as arguments.
+
+(add-hook
+ 'evr-file-claim-saved-hook
+ (lambda (seed-ref claim-ref)
+   …
+))
+"
+  :group 'evr
+  :type 'hook)
+
 ;;;###autoload
 (defun evr-save-file ()
-  "Saves the current buffer as file in everarch."
+  "Saves the current buffer as file in everarch.
+
+Returns t if the claim-set was successfully saved."
   (interactive)
-  (let ((file-buffer-name (buffer-name)))
+  (let ((file-buffer-name (buffer-name))
+        (seed-ref (if (local-variable-p 'evr-seed-ref) evr-seed-ref)))
     (message "Saving evr file %s..." file-buffer-name)
     (let ((proc (make-process
                  :name (concat "evr post-file " file-buffer-name)
                  :buffer (get-buffer-create "*evr post-file errors*")
-                 ;; TODO use buffer's file name instead of buffer-name
                  :command
                  (nconc
                   `("evr" "post-file" "--title" ,file-buffer-name)
-                  (if (local-variable-p 'evr-seed-ref)
-                      `("--seed" ,evr-seed-ref)
+                  (if seed-ref
+                      `("--seed" ,seed-ref)
                     ()))
                  :filter
                  (lambda (proc out)
                    (with-current-buffer file-buffer-name
-                     (if (not (local-variable-p 'evr-seed-ref))
-                         (setq-local evr-seed-ref (string-trim out)))
+                     (setq-local evr-claim-ref (string-trim out))
+                     (if (not seed-ref)
+                         (setq-local evr-seed-ref evr-claim-ref))
                      (evr-file-claim-mode t)
-                     (set-buffer-modified-p nil))
+                     (set-buffer-modified-p nil)
+                     (if (not seed-ref)
+                         (mapc
+                          (lambda (hook)
+                            (funcall hook nil evr-claim-ref))
+                          evr-seed-file-claim-saved-hook))
+                     (mapc
+                      (lambda (hook)
+                        (funcall hook evr-seed-ref evr-claim-ref))
+                      evr-file-claim-saved-hook))
                    (message "Wrote %s" file-buffer-name)
                    nil)
                  :stderr (get-buffer-create "*evr post-file errors*")
@@ -244,8 +348,46 @@ evr-attr-index and prints the results into a new buffer."
       ))
   t)
 
-(defun evr-enable-file-claim-mode ()
-  (add-hook 'write-contents-functions 'evr-save-file nil t))
+(defcustom evr-claim-set-saved-hook nil
+  "Run after `evr-save-claim-set' saved a claim set.
 
-(defun evr-disable-file-claim-mode ()
-  (remove-hook 'write-contents-finctions 'evr-save-file t))
+The claim set's ref is passed as argument.
+
+(add-hook
+ 'evr-claim-set-saved-hook
+ (lambda (claim-set-ref)
+   …
+))
+"
+  :group 'evr
+  :type 'hook)
+
+;;;###autoload
+(defun evr-save-claim-set ()
+  "Saves the current buffer as claim set into everarch.
+
+Returns t if the claim-set was successfully saved."
+  (interactive)
+  (let ((claim-set-buffer-name (buffer-name)))
+    (message "Saving evr claim set...")
+    (let ((proc (make-process
+                 :name (concat "evr sign-put " claim-set-buffer-name)
+                 :buffer (get-buffer-create "*evr put claim-set errors*")
+                 :command `("evr" "sign-put")
+                 :filter
+                 (lambda (proc out)
+                   (with-current-buffer claim-set-buffer-name
+                     (let ((claim-set-ref (string-trim out)))
+                       (set-buffer-modified-p nil)
+                       (mapc
+                        (lambda (hook)
+                          (funcall hook claim-set-ref))
+                        evr-claim-set-saved-hook)
+                       (message "Wrote claim-set %s" claim-set-ref)))
+                   nil)
+                 :stderr (get-buffer-create "*evr put claim-set errors*")
+                 )))
+      (process-send-region proc (point-min) (point-max))
+      (process-send-eof proc)
+      ))
+  t)
