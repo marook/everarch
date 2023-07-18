0db603ff060220cfa0326a9d83f98cc92fcda8f5
Markus Peröbner <markus.peroebner@gmail.com>
2023-07-18 21:00:46 +0200

    only execute unit tests with valgrind if it's available

--- test.sh
+++ test.sh
@@ -4,24 +4,28 @@ set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "${script_dir}"
+. 'test.conf'
 
 echo "Run unit tests…"
 make check
 
-echo ''
-echo "Run unit tests with valgrind…"
-for test_bin in src/*-test
-do
-    test_name=`basename "${test_bin}"`
-    if [[ "${test_name}" == "concurrent-glacier-test" ]]
-    then
-        # the tests above are currently excluded from the valgrind
-        # runs because they run too slow.
-        continue
-    fi
-    echo "Testing ${test_name}…"
-    ( cd src && valgrind --quiet --leak-check=yes --error-exitcode=1 "./${test_name}" )
-done
+if [[ "${HAS_VALGRIND}" == 'true' ]]
+then
+   echo ''
+   echo "Run unit tests with valgrind…"
+   for test_bin in src/*-test
+   do
+       test_name=`basename "${test_bin}"`
+       if [[ "${test_name}" == "concurrent-glacier-test" ]]
+       then
+           # the tests above are currently excluded from the valgrind
+           # runs because they run too slow.
+           continue
+       fi
+       echo "Testing ${test_name}…"
+       ( cd src && valgrind --quiet --leak-check=yes --error-exitcode=1 "./${test_name}" )
+   done
+fi
 
 echo ''
 echo 'Run integration tests…'
