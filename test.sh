9915db2fc46ec0f17429fb5878f709aa1c08c5ee
Markus Peröbner <markus.peroebner@gmail.com>
2023-11-12 09:27:09 +0100

    move integration test execution to Makefiles
    from test.sh. that should execute the integration tests before the
    podman images are built. the integration tests should be executed
    before podman because the podman builds also execute the integration
    test. the feedback loop is much smaller if the integration tests fail
    on the host system than when after building an podman image and
    running the container.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${script_dir}"
 . 'test.conf'
 
-echo "Run unit tests…"
+echo "Run unit and integration tests…"
 make check
 
 if [[ "${HAS_VALGRIND}" == 'true' ]]
@@ -28,30 +28,4 @@ then
 fi
 
 echo ''
-echo 'Run integration tests…'
-rm -f "testing/suite/put-parallel/glacier-benchmark.log"
-echo -n '' > "${script_dir}/test.log"
-for test_suite in testing/suite/*
-do
-    if [[ ! -d "${test_suite}" ]]
-    then
-        continue
-    fi
-    test_name=`basename "${test_suite}"`
-    echo "Run integration test ${test_name}…"
-    test_error=0
-    ( cd "${test_suite}" && ./run-suite ) &>> "${script_dir}/test.log" || test_error=1
-    if [[ "${test_error}" == '1' ]]
-    then
-        tail "${script_dir}/test.log" >&2
-        echo "
-
-For complete failure details see ${script_dir}/test.log" >&2
-        exit 1
-    fi
-done
-
-"${script_dir}/etc/profile-aggregate/profile-aggregate.py" < "${script_dir}/test.log"
-cat "testing/suite/put-parallel/glacier-benchmark.log"
-
 echo SUCCESS
